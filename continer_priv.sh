1485f3dea4e752bdd5716f38eae9ec70cd4cd517
dimitar.shenkov <dimitar_shenkov@epam.com>
2020-08-01 18:14:34 +0300

    reports

--- continer_priv.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-CONTAINER_NAME=$1
-if [[ $(docker inspect --format='{{.HostConfig.Privileged}}' $CONTAINER_NAME) == "false" ]]; then
-    echo not privileged
-else
-    echo privileged
-fi
