7ee20ff978029370bf45ef53047df3d03fab6e06
dimitar.shenkov <dimitar_shenkov@epam.com>
2020-08-01 17:11:27 +0300

    first test

---
+++ jsweb/tests/test_priv.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+ip link add dummy0 type dummy >/dev/null
+if [[ $? -eq 0 ]]; then
+    PRIVILEGED=true
+    # clean the dummy0 link
+    ip link delete dummy0 >/dev/null
+    echo privileged
+    exit 1
+else
+    echo not privileged
+    PRIVILEGED=false
+fi
