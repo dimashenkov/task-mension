0e5bef81ba17730578c989a7f2ea7768b455a6af
dimitar.shenkov <dimitar_shenkov@epam.com>
2020-08-01 18:00:03 +0300

    2 test

---
+++ jsweb/tests/test_kernel_v.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+kernel_version=`uname -a|awk '{print $3}' | sed 's/[^0-9]*//g'`
+kernel_version2=`uname -a|awk '{print $3}' | cut -f1 -d"-"`
+if [ $kernel_version -ge 41976 ]
+then
+    echo "kernel version is $kernel_version2"
+else
+    echo kernel version is less then 4.19.76
+    exit 1
+fi
