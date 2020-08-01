592c1152851957db5071a3821e604bc121f9764b
dimitar.shenkov <dimitar_shenkov@epam.com>
2020-08-01 21:04:07 +0300

    clean workspace

--- jsweb/tests/first_script.sh
+++ jsweb/tests/first_script.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
-arr=()
+errors=()
 for f in /tmp/tests/test*; do  
-  bash "$f" -H 
+  bash "$f" -H
+  if [ $? -eq 0 ]; then
+      echo OK
+  else
+      errors+=("$f")
+  fi 
 done
+
+if [ ${#errors[@]} -eq 0 ]; then
+    echo "No errors, hooray"
+else
+    echo "Oops, something went wrong..."
+fi
