97bed23f7b925cd61ff5c27b67a41113fe489e76
dimitar.shenkov <dimitar_shenkov@epam.com>
2020-08-01 19:02:23 +0300

    clean workspace

--- jsweb/tests/test_kernel_v.sh
+++ jsweb/tests/test_kernel_v.sh
@@ -3,9 +3,12 @@ kernel_version=`uname -a|awk '{print $3}' | sed 's/[^0-9]*//g'`
 kernel_version2=`uname -a|awk '{print $3}' | cut -f1 -d"-"`
 if [ $kernel_version -ge 41976 ]
 then
+	echo -e ""
 	echo -e "=== Test result kernel version==="
+	echo "Kernel version is bigger then 4.18" 
     echo "Kernel version is $kernel_version2" 
 else
+	echo -e ""
 	echo -e "=== Test result kernel version==="
     echo Kernel version is less then 4.19.76
     exit 1
--- jsweb/tests/test_priv.sh
+++ jsweb/tests/test_priv.sh
@@ -4,11 +4,13 @@ if [[ $? -eq 0 ]]; then
     PRIVILEGED=true
     # clean the dummy0 link
     ip link delete dummy0 >/dev/null
+    echo -e ""
     echo -e "=== Test result container privileged mode==="
     echo This container is in privileged mode !!!
     exit 1
 else
+	echo -e ""
 	echo -e "=== Test result container privileged mode==="
-    echo This container is not privileged mode
+    echo This container is not in privileged mode
     PRIVILEGED=false
 fi
