8678b6710bfad4c7bee96180ab55f8400872dc0d
dimitar.shenkov <dimitar_shenkov@epam.com>
2020-08-03 13:26:14 +0300

    test 4

--- jsweb/tests/test_kernel_v.sh
+++ jsweb/tests/test_kernel_v.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 kernel_version=`uname -a|awk '{print $3}' | sed 's/[^0-9]*//g'`
 kernel_version2=`uname -a|awk '{print $3}' | cut -f1 -d"-"`
-if [ $kernel_version -ge 419761 ]
+target_kernel_version=419761
+if [ $kernel_version -ge $target_kernel_version ]
 then
 	echo -e ""
 	echo -e "================================================================================"
@@ -12,6 +13,6 @@ else
 	echo -e ""
 	echo -e "================================================================================"
 	echo -e "=== Test result kernel version==="
-    echo ERROR Kernel version is less then 4.19.76
+    echo "ERROR Kernel version is less then $target_kernel_version"
     exit 1
 fi
